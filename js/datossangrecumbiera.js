40a8ca5d395685f28db6fd51713d9a8cc2f48fff
AvidezLP <121735056+AvidezLP@users.noreply.github.com>
2026-06-01 11:48:11 -0400

    Add files via upload

--- js/datossangrecumbiera.js
+++ js/datossangrecumbiera.js
@@ -1,6 +1,6 @@
 const sangreCumbiera = [
     {
-        titulo: "Crucificame - Sangre Cumbiera",
+        titulo: "Crucificame",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -10,7 +10,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/vtfug9mqiu130zs/Crucificame_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Los Amantes - Sangre Cumbiera",
+        titulo: "Los Amantes",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -20,7 +20,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/m0in6d3qs9qcmcu/Los_Amantes_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Peligroso Amor - Sangre Cumbiera",
+        titulo: "Peligroso Amor",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -30,7 +30,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/5pj58fot121v70j/Peligroso_Amor_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Nada Se Compara Contigo - Sangre Cumbiera",
+        titulo: "Nada Se Compara Contigo",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -40,7 +40,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/vj5lkxetrh6phed/Nada_Se_Compara_Contigo_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Estrechandome - Sangre Cumbiera",
+        titulo: "Estrechandome",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -50,7 +50,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/idqk5ispqulw5nk/Estrechandome_-_Sangre_Cumbiera_Instrumental.mp3/file"
     },
     {
-        titulo: "Quitame La Vida - Sangre Cumbiera",
+        titulo: "Quitame La Vida",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -60,7 +60,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/4k36g88rbcm67s1/Quitame_La_Vida_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Nuestro Juramento - Sangre Cumbiera",
+        titulo: "Nuestro Juramento",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -70,7 +70,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/g9gog6vmzcot1vz/Nuestro_Juramento_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Virgen - Sangre Cumbiera",
+        titulo: "Virgen",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -80,7 +80,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/82jon1kxnecl9t4/Virgen_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Ódiame - Sangre Cumbiera",
+        titulo: "Ódiame",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -90,7 +90,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/4zantr8hixjpcvt/%25C3%2593diame_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Propiedad Privada - Sangre Cumbiera",
+        titulo: "Propiedad Privada",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -100,7 +100,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/tgfas6ah8lea8vp/Propiedad_Privada_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Veneno - Sangre Cumbiera",
+        titulo: "Veneno",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -110,7 +110,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/zoaorggzfi40z9z/Veneno_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Se Fue Llorando - Sangre Cumbiera",
+        titulo: "Se Fue Llorando",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -120,7 +120,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/3m74aub8h4g0qwd/Se_Fue_Llorando_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Pequeño Motel - Sangre Cumbiera",
+        titulo: "Pequeño Motel",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -130,7 +130,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/y5mtfndr6a68g1h/Peque%25C3%25B1o_Motel_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Amor Sincero - Sangre Cumbiera",
+        titulo: "Amor Sincero",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -140,7 +140,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/tv3095cthrhyhr8/Amor_Sincero_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "No Me Dejes - Sangre Cumbiera instrumental",
+        titulo: "No Me Dejes",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -150,7 +150,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/ghnlmz95msmxhjd/No_Me_Dejes_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Amarte Hasta La Muerte - Sangre Cumbiera",
+        titulo: "Amarte Hasta La Muerte",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -160,7 +160,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/2cgidjjvlmj88k7/Amarte_Hasta_La_Muerte_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Esclavo Y Amo - Sangre Cumbiera",
+        titulo: "Esclavo Y Amo",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -170,7 +170,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/btrph79i99gbk3w/Esclavo_Y_Amo_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Pagarás - Sangre Cumbiera",
+        titulo: "Pagarás",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -180,7 +180,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/ztqws2p2c8pdz93/Pagar%25C3%25A1s_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Adiós Amor - Sangre Cumbiera",
+        titulo: "Adiós Amor",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -190,7 +190,7 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/mu8g4ag9lu3fwhm/Adi%25C3%25B3s_Amor_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "Coralí - Sangre Cumbiera",
+        titulo: "Coralí",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
@@ -210,93 +210,83 @@ const sangreCumbiera = [
         descarga: "https://www.mediafire.com/file/v4btmkzwzygunut/Eh_Nacido_Para_Amarte_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "La Revancha",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "3:07",
+        descarga: "https://www.mediafire.com/file/vlx8i6s2rsbnjc5/La_Revancha_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "Que Mal Me Pagas",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "3:16",
+        descarga: "https://www.mediafire.com/file/2g7z7bknpspvovb/Que_Mal_Me_Pagas_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "Con La Misma Moneda",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "4:18",
+        descarga: "https://www.mediafire.com/file/6b1us77gku6awjq/Con_La_Misma_Moneda_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "Amor Añejo",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "3:25",
+        descarga: "https://www.mediafire.com/file/wp3jy7wnfdqli4f/Amor_A%25C3%25B1ejo_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "Gracias",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "2:46",
+        descarga: "https://www.mediafire.com/file/6wo60h1czy791r9/Gracias_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "Par De Anillos",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "3:47",
+        descarga: "https://www.mediafire.com/file/ffvt5f9aoxqmwbm/Par_De_Anillos_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "Que Locura Enamorarme De Ti",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "3:07",
+        descarga: "https://www.mediafire.com/file/omdfhtfnp9mzslr/Que_Locura_Enamorarme_De_Ti_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
     {
-        titulo: "",
+        titulo: "Amor De Primavera",
         artista: "Sangre Cumbiera",
         grupoId: "sangre-cumbiera",
         genero: "Cumbia con Sabor",
         tono: "-",
         bpm: "98",
-        duracion: "",
-        descarga: ""
-    },
-    {
-        titulo: "",
-        artista: "Sangre Cumbiera",
-        grupoId: "sangre-cumbiera",
-        genero: "Cumbia con Sabor",
-        tono: "-",
-        bpm: "98",
-        duracion: "",
-        descarga: ""
+        duracion: "2:28",
+        descarga: "https://www.mediafire.com/file/4zyw9k95zx91ar6/Amor_De_Primavera_-_Sangre_Cumbiera_instrumental.mp3/file"
     },
 ];
