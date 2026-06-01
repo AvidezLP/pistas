02ec3c6bbaceb9840a857452464fd189d46df6f2
AvidezLP <121735056+AvidezLP@users.noreply.github.com>
2026-06-01 01:09:50 -0400

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -31,15 +31,54 @@ const pistas = [
         descarga: "https://www.mediafire.com/file/dv9fkn0y1eihxjv/MiX_Cuatro_Palabras_Bebiendo_L%25C3%25A1grimas_Para_Que_Quiero_Un_Coraz%25C3%25B3n_Amor_Se_Escribe_con_Llanto_-_Explosi%25C3%25B3n_Cumbiera.mp3/file"
     },
     {
-        titulo: "Ejemplo - Cumbia con Sabor 2",
+        titulo: "MiX Amor Se Escribe Con Llanto - Explosión Cumbiera",
         artista: "Explosión Cumbiera",
         grupoId: "explosion-cumbiera",
         genero: "Cumbia con Sabor",
-        tono: "Solm",
-        bpm: "98",
-        duracion: "3:58",
-        demo: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
-        descarga: "https://www.mediafire.com/"
+        tono: "",
+        bpm: "93",
+        duracion: "1:09",
+        descarga: "https://www.mediafire.com/file/2lov5l2lxdbb3om/MiX_Amor_Se_Escribe_Con_Llanto_-_Explosi%25C3%25B3n_Cumbiera.mp3/file"
+    },
+    {
+        titulo: "MiX Bebiendo Lágrimas - Explosión Cumbiera",
+        artista: "Explosión Cumbiera",
+        grupoId: "explosion-cumbiera",
+        genero: "Cumbia con Sabor",
+        tono: "",
+        bpm: "93",
+        duracion: "1:44",
+        descarga: "https://www.mediafire.com/file/as94jtnxnpxcupv/MiX_Bebiendo_L%25C3%25A1grimas_-_Explosi%25C3%25B3n_Cumbiera.mp3/file"
+    },
+    {
+        titulo: "MiX Cuatro Palabras - Explosión Cumbiera",
+        artista: "Explosión Cumbiera",
+        grupoId: "explosion-cumbiera",
+        genero: "Cumbia con Sabor",
+        tono: "",
+        bpm: "93",
+        duracion: "1:43",
+        descarga: "https://www.mediafire.com/file/u0x3k8486dq5etd/MiX_Cuatro_Palabras_-_Explosi%25C3%25B3n_Cumbiera.mp3/file"
+    },
+    {
+        titulo: "MiX Historia De Un Amor - Explosión Cumbiera",
+        artista: "Explosión Cumbiera",
+        grupoId: "explosion-cumbiera",
+        genero: "Cumbia con Sabor",
+        tono: "",
+        bpm: "93",
+        duracion: "2:04",
+        descarga: "https://www.mediafire.com/file/j2kw3374yz3bdj6/MiX_Historia_De_Un_Amor_-_Explosi%25C3%25B3n_Cumbiera.mp3/file"
+    },
+    {
+        titulo: "MiX Para Que Quiero Un Corazón - Explosión Cumbiera",
+        artista: "Explosión Cumbiera",
+        grupoId: "explosion-cumbiera",
+        genero: "Cumbia con Sabor",
+        tono: "",
+        bpm: "93",
+        duracion: "1:53",
+        descarga: "https://www.mediafire.com/file/9pj3sunhe2nqptw/MiX_Para_Que_Quiero_Un_Coraz%25C3%25B3n_-_Explosi%25C3%25B3n_Cumbiera.mp3/file"
     },
     {
         titulo: "Ejemplo - Cumbia Sureña",
