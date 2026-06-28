a8060761550f183c852eb509a01266f46759ff13
AvidezLP <121735056+AvidezLP@users.noreply.github.com>
2026-06-27 22:34:19 -0400

    Add files via upload

--- js/cumbia-con-sabor/onda-kumbiera.js
+++ js/cumbia-con-sabor/onda-kumbiera.js
@@ -7,7 +7,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:06",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/dyjvgxfj2zu5yfg/1_Onda_Kumbiera_%2526_Diego_Soria_-_Yo_no_soy_tu_marido_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "La Cancioncita",
@@ -17,7 +17,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:01",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/bxm8k6qsjnt8fuz/2_Onda_Kumbiera_-_La_Cancioncita_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Hoy Lloras Por Mi",
@@ -27,7 +27,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:17",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/xni4mfhr5fktkuk/3_Onda_Kumbiera_-_Hoy_Lloras_por_M%25C3%25AD_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Hoy Se Bebe",
@@ -37,7 +37,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:37",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/g0rq8rri05vkrzo/4_Onda_Kumbiera_-_Hoy_Se_Bebe_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Devuélveme",
@@ -47,7 +47,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:19",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/yolkze4qzc4b5en/5_Onda_Kumbiera_-_Devu%25C3%25A9lveme_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Dejame Un Beso",
@@ -57,7 +57,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:28",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/b78cn7tz31okzmp/6_Onda_Kumbiera_-_Dejame_Un_Beso_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Corazón De Madera",
@@ -67,7 +67,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:29",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/akq8ds7e6s40gwn/7_Onda_Kumbiera_-_Corazon_de_Madera%2521_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Dios Mio",
@@ -77,7 +77,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:17",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/dqvwcmfvn09fdjk/8_Daniel_Cardozo_%2526_Onda_Kumbiera_-_Dios_M%25C3%25ADo_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Por Tu Culpa",
@@ -87,7 +87,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:15",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/g1c19mp9e5y9jb7/9_Onda_Kumbiera_-_Por_Tu_Culpa_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Quiéreme",
@@ -97,7 +97,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:48",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/21xcr3w4gx1ndo2/10_Onda_Kumbiera_-_Qui%25C3%25A9reme_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Pagarás",
@@ -107,7 +107,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:55",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/3mctiesgkxe2s3v/11_Onda_Kumbiera_-_Pagar%25C3%25A1s_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Luisa Molina",
@@ -117,7 +117,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "6:39",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/m6jca55a5llzh8t/12_Onda_Kumbiera_%2526_Diego_Soria_-_Mix_Luisa_Molina_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Ceviche",
@@ -127,7 +127,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:13",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/s703m06njrsym9n/13_Onda_Kumbiera_-_Mix_Ceviche_%2528feat._Diego_Soria%2529_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Cuando Quieras Déjame",
@@ -137,7 +137,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:03",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/iow5nobvm7a70sj/14_Onda_Kumbiera_-_Cuando_Quieras_D%25C3%25A9jame_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Te Quise Olvidar",
@@ -147,7 +147,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:27",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/txv5fkh8w5wpynw/15_Onda_Kumbiera_-_Te_Quise_Olvidar_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Yo No Fui",
@@ -157,7 +157,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:23",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/b9p20o1ird8bu7u/16_Onda_Kumbiera_%2526_Diego_Soria_-_Yo_No_Fui_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "No Puedo Vivir Sin Ti",
@@ -167,7 +167,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:10",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/zbwt1nivstgzqgo/17_Onda_Kumbiera_-_No_Puedo_Vivir_Sin_Ti_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Pitty Zapata",
@@ -177,7 +177,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "7:32",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/l9af21tomtqy5ig/18_Onda_Kumbiera_-_Mix_Pitty_Zapata_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Falsa Mujer",
@@ -187,7 +187,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:30",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/lv4741efer6hdgi/19_Onda_Kumbiera_-_Falsa_Mujer_%2528feat._Segundo_Rosero%2529_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mal Acostumbrado",
@@ -197,7 +197,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:04",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/u559dc3ba8m6qsu/20_Onda_Kumbiera_-_Mal_Acostumbrado_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Tatto",
@@ -207,7 +207,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:27",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/55bh18okmeo3x71/21_Onda_Kumbiera_-_Tatto_%2528feat._Diego_Soria%2529_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Climax 2 Amiga / Traicionera / Jamás",
@@ -217,7 +217,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "5:24",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/mcqm5zkt5r7qsrj/22_Onda_Kumbiera_%2526_Diego_Soria_-_Mix_Climax2__Amiga_Traicionera_Jam%25C3%25A1s_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Enlace No Me Vuelvo A Enamorar / Quien Dime QUién / Ojitos Negros",
@@ -227,7 +227,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:50",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/5gjmhay8g5k5mea/23_Onda_Kumbiera_-_Mix_Enlace__No_Me_Vuelvo_a_Enamorar_Dime_Qui%25C3%25A9n_Ojitos_Negros_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "El Teléfono",
@@ -237,7 +237,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:15",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/6ychdcm3u2lg68f/24_Onda_Kumbiera_-_El_Tel%25C3%25A9fono_%2528feat._Segundo_Rosero%2529_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Minero",
@@ -247,7 +247,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "6:21",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/8nqtor4pdmpp4iv/25_Onda_Kumbiera_-_Mix_Minero_%2528feat._Diego_Soria%2529_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Jamás Podré Olvidar",
@@ -257,7 +257,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "2:54",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/tjgsp32jrli1q5o/26_Onda_Kumbiera_-_Jam%25C3%25A1s_Podr%25C3%25A9_Olvidar_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Kumbiero: No Me Mientas / Duele",
@@ -267,7 +267,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "5:15",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/vjhsnwal4r1d4py/27_Onda_Kumbiera_-_Mix_Kumbiero__No_Me_Mientas_Duele_%2528feat._David_Castro_y_America%2529_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Yo No Te Hago Falta",
@@ -277,7 +277,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:10",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/0juo0ki9f098qhr/28_Onda_Kumbiera_-_Yo_No_Te_Hago_Falta_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Perfume",
@@ -287,7 +287,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "2:31",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/s8tgtekq1fzdd7p/29_Onda_Kumbiera_-_Perfume_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Inolvidable",
@@ -297,7 +297,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "2:34",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/ifjerryayi3k9k4/30_Onda_Kumbiera_-_Inolvidable_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Bonito",
@@ -307,7 +307,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:26",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/r5600mvxlmc8tvt/31_Onda_Kumbiera_-_Mix_Bonito_%2528feat._Diego_Soria%2529_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Te Acuerdas De Mi",
@@ -317,7 +317,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "4:09",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/62wr9bl9s029fol/32_Onda_Kumbiera_-_Te_Acuerdas_de_Mi_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Climax",
@@ -327,7 +327,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "5:21",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/o8dpkhwzgmmhht0/33_Onda_Kumbiera_-_Mix_Climax_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Libre, Solterito y Sin Nadie",
@@ -337,7 +337,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "3:18",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/k0ji3j6kmc8r40a/34_Onda_Kumbiera_-_Libre%252C_Solterito_y_Sin_Nadie_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Se Que Te Amaré",
@@ -347,7 +347,7 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "2:37",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/4r0poytd5ymqjfa/35_Onda_Kumbiera_%2526_Diego_Soria_-_Se_Que_Te_Amare_%2528Instrumental%2529.mp3/file"
     },
     {
         titulo: "Mix Morena",
@@ -357,6 +357,6 @@ const ondakumbiera = [
         tono: "-",
         bpm: "",
         duracion: "6:29",
-        descarga: ""
+        descarga: "https://www.mediafire.com/file/pd7vqrd2h7a8hr9/36_Onda_Kumbiera_-_Mix_Morena_%2528feat._Diego_Soria%2529_%2528Instrumental%2529.mp3/file"
     },
 ];
